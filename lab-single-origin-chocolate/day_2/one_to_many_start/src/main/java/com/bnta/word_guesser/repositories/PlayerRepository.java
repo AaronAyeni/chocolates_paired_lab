317d3c0ccdb9bf0927b6fdb136f50f56a011e100
KhanZuh <khanzuhair96@live.co.uk>
2024-06-04 15:39:43 +0100

    FIle structure fixed

--- lab-single-origin-chocolate/day_2/one_to_many_start/src/main/java/com/bnta/word_guesser/repositories/PlayerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bnta.word_guesser.repositories;
-
-import com.bnta.word_guesser.models.Player;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlayerRepository extends JpaRepository<Player, Long> {
-}
-
-//a lot of Jpa stuff is like save and build, already built into it
