7f5c3e9abd28044e78d52d6bb8679a50f484e158
Aaron Ayeni <aaronayeni135@gmail.com>
2024-06-04 15:51:34 +0100

    Estate class complete

--- lab-single-origin-chocolate/chocolate_start_point/src/main/java/com/bnta/chocolate/models/Estate.java
+++ lab-single-origin-chocolate/chocolate_start_point/src/main/java/com/bnta/chocolate/models/Estate.java
@@ -7,19 +7,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-
+@Entity
+@Table(name = "estates")
 public class Estate {
 
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-
+    @Column(name = "name")
     private String name;
 
-
+    @Column(name = "country")
     private String country;
 
-
+    @ManyToOne
+    @JoinColumn(name = "chocolate_id")
+    @JsonIgnoreProperties({"estates"})
     private List<Chocolate> chocolates;
 
     public Estate(String name, String country) {
